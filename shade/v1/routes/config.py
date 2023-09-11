e14ed88acd7ab7488f541acadde6118be6865075
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2023-09-10 20:13:55 -0400

    add config set route

--- shade/v1/routes/config.py
+++ shade/v1/routes/config.py
@@ -12,3 +12,11 @@ class Config:
     def get_config(self) -> dict:
         # TODO make this a pydantic model
         return self.__api.get('config').json()
+
+    def set_config(self, config: dict) -> None:
+        """
+        Get the config using the get route, modify it, then set it using this
+        :param config: The config dict to set
+        :return: None
+        """
+        self.__api.post('config', json=config)
