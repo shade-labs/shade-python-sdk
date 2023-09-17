f69c05dbf059ed16cdff1050b2c60b617bab4178
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2023-09-16 23:08:42 -0400

    add get asset by path

--- shade/v1/routes/assets.py
+++ shade/v1/routes/assets.py
@@ -5,6 +5,7 @@ import uuid
 from shade.v1.api import API
 from shade.v1.types import MountInfo
 from shade.v1.models import AssetModel
+from pathlib import Path
 from typing import List
 
 
@@ -20,5 +21,12 @@ class Assets:
 
     def get_asset_by_id(self, id_: uuid.UUID) -> AssetModel:
         asset = self.__api.get(f'assets/{id_}')
+
+        return AssetModel(**asset.json())
+
+    def get_asset_by_path(self, path: Path) -> AssetModel:
+        asset = self.__api.get(f'indexing/file', params={
+            'path': str(path)
+        })
 
         return AssetModel(**asset.json())
