cc246049965fbf21519dd02d822b09d0f9296e4c
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2024-01-04 21:47:55 +1300

    add collections class

--- shade/v1/routes/collections.py
+++ shade/v1/routes/collections.py
@@ -1,3 +1,13 @@
 # TODO collections an asset belongs in, collection info (/collections/collection_id), list all
 #  collections, delete collection, add assets to collection, delete assets from collection
-#
+from shade.v1.api import API
+from shade.v1.types import MountInfo
+
+
+class Collections:
+    def __init__(self, api: API, mount_info: MountInfo):
+        self.__api = api
+        self.__mount_info = mount_info
+
+    def get_collections(self):
+        return self.__api.get('collections').json()
