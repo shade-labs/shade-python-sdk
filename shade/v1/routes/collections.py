8b9ce29803e135b3b5a2629c96b9bd3f80b2c33a
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2024-01-06 23:12:55 +1300

    add get collection by id

--- shade/v1/routes/collections.py
+++ shade/v1/routes/collections.py
@@ -2,7 +2,7 @@
 #  collections, delete collection, add assets to collection, delete assets from collection
 from shade.v1.api import API
 from shade.v1.types import MountInfo
-
+import uuid
 
 class Collections:
     def __init__(self, api: API, mount_info: MountInfo):
@@ -11,3 +11,6 @@ class Collections:
 
     def get_collections(self):
         return self.__api.get('collections').json()
+
+    def get_collection_by_id(self, collection_id: uuid.UUID):
+        return self.__api.get(f'collections/{collection_id}')
