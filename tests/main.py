74fc865c414c1a119b92f679f37f6b1d279c8c49
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2024-02-12 21:13:19 -0500

    0.2.17 release

--- tests/main.py
+++ tests/main.py
@@ -4,6 +4,7 @@ import sys
 import pytest
 
 
+# This does NOT work on windows, it just doesn't seem to init the conftest
 def main():
     code = pytest.main(args=[
         os.path.dirname(os.path.abspath(__file__)),
