3773e0bae3ef41691934ed2ffc030a8f0d4a2d4f
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2024-10-19 00:37:13 -0400

    add mv

--- shade/v2/resources/file.py
+++ shade/v2/resources/file.py
@@ -16,3 +16,26 @@ class File(ABCResource):
                              })
 
         resp.raise_for_status()
+
+    def mv(self, drive: UUID | dict, source: Path, destination: Path):
+        """
+        Move a folder or file to one location to another. The destination folder must exist and the destination file
+        must not already exist
+
+        :param drive: drive uuid
+        :param source: the file or folder to move from
+        :param destination: to location to move it to
+        :return: None
+        """
+        if isinstance(drive, dict):
+            drive = drive.get('id')
+
+        resp = requests.post(self.auth.remote_url + '/files/move',
+                             headers={'Authorization': self.auth.api_key},
+                             json={
+                                 'drive_id': drive,
+                                 'source': f'/{drive}{source}',
+                                 'destination': f'{drive}{destination}'
+                             })
+
+        resp.raise_for_status()
