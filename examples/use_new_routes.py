24cca0d896d13003d00ab82e3e14a4887278c44e
Sebastian Bastidas <sbastidasr@gmail.com>
2024-11-20 12:50:11 -0500

    Shared file w user

--- examples/use_new_routes.py
+++ examples/use_new_routes.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from shade.v2 import Shade
+from shade.v2.resources.share import DriveRole
 
 REMOTE_URL = 'http://127.0.0.1:9082'
 
@@ -12,18 +13,26 @@ if __name__ == '__main__':
     # workspaces = shade.workspace.get_workspaces()
     workspace = shade.workspace.get_workspace_by_domain('shade-workspace')
 
-    print(workspace)
+    # print('workspace', workspace)
 
     drives = shade.drive.get_drives(workspace)
 
     drive = drives[0]
 
-    assets = shade.asset.listdir_files(
-        drive=drive.get('id'), path=Path('/'), page=0, limit=100
-    )
-    print(assets)
+    assets = shade.asset.listdir_files(drive=drive, path=Path('/'), page=0, limit=100)
+    # print('assets', assets)
 
     folders = shade.asset.listdir_folders(
         drive=drive.get('id'), path=Path('/'), page=0, limit=100
     )
-    print(folders)
+    # print(folders)
+
+    if shade.share.share_asset(
+        drive=drive,
+        asset_path=Path(assets[0].get('path')),
+        email='emerson@shade.inc',
+        role=DriveRole.EDITOR,
+        url='https://shade.inc',
+        message='Hello',
+    ):
+        print('Shared file with user')
---
+++ shade/v2/resources/share.py
@@ -0,0 +1,53 @@
+from enum import Enum
+from pathlib import Path
+from uuid import UUID
+
+import requests
+
+from .abc_resource import ABCResource
+
+
+class DriveRole(int, Enum):
+    ADMIN = 50
+    FULL_ACCESS = 40
+    EDITOR = 30
+    COMMENTER = 20
+    DOWNLOADER = 17
+    VIEWER = 15
+    METADATA_VIEWER = 10
+    NONE = 0
+
+
+class Share(ABCResource):
+    def share_asset(
+        self,
+        drive: UUID | dict,
+        asset_path: Path,
+        email: str,
+        role: DriveRole,
+        url,
+        message,
+    ) -> dict:
+        if isinstance(drive, dict):
+            drive = drive.get('id')
+
+        json = {
+            'path': str(asset_path),
+            'invites': [
+                {
+                    'email': email,
+                    'role': role.value,
+                }
+            ],
+            'url': url,
+            'message': message,
+        }
+        print(json)
+
+        resp = requests.post(
+            self.auth.remote_url + f'/workspaces/drives/{drive}/share-file',
+            headers={'Authorization': self.auth.api_key},
+            json=json,
+        )
+        resp.raise_for_status()
+        return resp.json()
