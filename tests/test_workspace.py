e0c04954c0cdf694c476e9e4b701f860b62b99a8
Sebastian Bastidas <sbastidasr@gmail.com>
2025-01-27 12:51:59 -0500

    Create Shade Fixture

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -1,3 +1,5 @@
+import pytest
+
 from shade import Shade
 from tests.helpers import get_random_string
 
@@ -6,10 +8,17 @@ API_KEY = 'sk_965a8a31cbf49c8ecc842082d40d5fa8cc529aa78a217788536cbeff5cf56ab3'
 REMOTE_URL = 'http://127.0.0.1:9082'
 
 
-shade = Shade(remote_url=REMOTE_URL, api_key=API_KEY)
+# shade = Shade(remote_url=REMOTE_URL, api_key=API_KEY)
 
 
-def test_create_and_get_workspace():
+@pytest.fixture
+def shade():
+    return Shade(remote_url=REMOTE_URL, api_key=API_KEY)
+
+
+def test_create_and_get_workspace(
+    shade: Shade,
+):
     """Test a crud route for a member to create a drive in a workspace"""
 
     workspace_data = {
